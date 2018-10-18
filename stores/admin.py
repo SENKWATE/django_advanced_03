60b368edcecb0f35e973f6d523ec177e371e4cae
SENKWATE <m_alsaiegh@hotmail.com>
2018-10-18 11:24:19 +0300

    advanced03 submission

--- stores/admin.py
+++ stores/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Store
 
-# Register your models here.
-admin.site.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name','description')
+    list_filter = (['added'])
+    search_fields = ['name', 'description']
+    list_display_links = (['name'])
+    list_editable = ['description']
+
+
+admin.site.register(Store,StoreAdmin)
